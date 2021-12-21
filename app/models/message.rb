9b3a95111fe9481592a5c4bf5b8ed3fd63e0f068
Benjamin Reynolds <breyno127@gmail.com>
2021-12-21 16:35:53 -0600

    feat: setup live updates using Turbo Streams

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :author, class_name: "User", foreign_key: :author_id
+  after_create_commit { broadcast_append_to self.conversation }
 end
